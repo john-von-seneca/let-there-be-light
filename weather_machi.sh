725048ca0d62b4fccc999d733857ae01d1cc78fe
jvs <john.von.seneca@gmail.com>
2015-12-31 16:55:19 +0530

    i3bar: weather

--- weather_machi.sh
+++ weather_machi.sh
@@ -5,4 +5,7 @@ URL='http://www.accuweather.com/en/in/bangalore/204108/weather-forecast/204108'
 #wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $2": "$16", "$12"°" }'| head -1
 
 
-wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $10"°C",$14}' | head -n 1
+# wget -q -O- "$URL" | awk -F\'
+# '/acm_RecentLocationsCarousel\.push/{print $10"°C",$14}' | head -n
+# 1
+echo 22C, Rain
