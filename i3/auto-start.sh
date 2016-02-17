2d955a8823e8828f579acdb262941cb721a5f35a
jvs <john.von.seneca@gmail.com>
2016-02-17 16:00:57 +0530

    i3: inc time, disable dsktp overlay

--- i3/auto-start.sh
+++ i3/auto-start.sh
@@ -3,14 +3,14 @@
 i3-msg 'workspace 1; append_layout ~/.i3/workspace-1.json'
 clementine &
 
-sleep 15
+sleep 40
 
 # workspace 2
 # clementine
 i3-msg 'workspace 2; append_layout ~/.i3/workspace-2.json'
 google-chrome-unstable &
 
-sleep 20
+sleep 40
 
 # workspace 4
 # emacs + terminal
@@ -18,7 +18,7 @@ i3-msg 'workspace 4; append_layout ~/.i3/workspace-4.json'
 emacs &
 gnome-terminal --title=foremacs &
 
-sleep 20
+sleep 40
 
 # workspace 5
 # terminal
@@ -30,7 +30,7 @@ i3-msg 'workspace 6; append_layout ~/.i3/workspace-6.json'
 
 # workspace 9
 # nautilus -- desktop window
-i3-msg 'workspace 9; append_layout ~/.i3/workspace-9.json'
+# i3-msg 'workspace 9; append_layout ~/.i3/workspace-9.json'
 nautilus &
 
 
