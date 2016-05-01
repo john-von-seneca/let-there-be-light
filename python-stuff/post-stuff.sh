aa0772fd3780c021802ad7ec6f98038da699dfe3
jvs <john.von.seneca@gmail.com>
2016-05-01 06:29:50 +0530

    python addons

---
+++ python-stuff/post-stuff.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+cd ~/neo-human/repos/IPython-notebook-extensions
+python3 setup.py install --user
+pip3 install --user --upgrade -r requirements.txt
+pip3 install --user --upgrade psutil widgetsnbextension
+pip3 install --user --upgrade ipython jupyter_client jupyter_core notebook nbconvert nbformat traitlets pyyaml
+python3 setup.py install --user
+cp -R nbextensions ~/.local/share/jupyter
+cp -R templates ~/.local/share/jupyter
+cp -R extensions ~/.local/share/jupyter
+
+
