8ac5c5adddffdc3fc462610545bab6042876cd27
jvs <john.von.seneca@gmail.com>
2016-05-01 06:29:21 +0530

    fstab stuff

---
+++ fstab-stuff.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+sudo mkdir /media/sulley
+sudo chown dragon:dragon /media/sulley
+
+sudo mkdir /media/mike
+sudo chown dragon:dragon /media/mike
+
+sudo mkdir /media/titan
+sudo chown dragon:dragon /media/titan
+
+sudo mkdir /media/ds
+sudo chown dragon:dragon /media/ds
+
+
