2ba5972732d8a264f114a6c66a7261b12ccf5d14
jvs <john.von.seneca@gmail.com>
2016-05-07 10:28:00 +0530

    linux: pkgs

--- linux/install-stuff.py
+++ linux/install-stuff.py
@@ -10,14 +10,14 @@ packages = [["automake", "axel"],
  ["clang", "clementine", "clementine", "cmake", "cmake-curses-gui", "cmake-curses-gui", "conky-all", "cowsay", "cscope", "cscope", "cuetools", "curl"],
  ["doxygen", "doxygen", "doxygen-gui", "doxygen-gui"],
  ["easytag", "emacs", "emacs-snapshot", "emacs-snapshot-el", "exfat-fuse", "exfat-fuse", "exfat-utils", "exfat-utils"], 
- ["feh", "firefox", "fortune-mod", "freeglut3"],
+ ["feh", "firefox", "fortune-mod", "freeglut3", "fslint"],
  ["g++", "gcc", "gedit", "git", "git-core", "global", "gnome-terminal", "gparted", "gradle", "grsync"],
  ["hddtemp", "hddtemp", "htop", "htop"],
  ["i3", "i3-wm", "i3-wm", "i3status", "ipython-notebook", "ipython-notebook", "ipython-notebook", "ipython-notebook", "ipython3", "ipython3", "ipython3-notebook", "ipython3-notebook"],
  ["jabref", "jabref", "jabref"],
  ["kcachegrind", "kdelibs5-data", "kdelibs5-data", "kdelibs5-plugins", "kdelibs5-plugins", "krusader"],
  ["libavcodec-dev", "libavformat-dev", "libboost-all-dev", "libdc1394-22-dev", "libeigen3-dev", "libfreetype6-dev", "libgtk2.0-dev"],
- ["libjasper-dev", "libjpeg-dev", "libpcre3", "libpython2.7", "libpython2.7", "libpython2.7-dev", "libpython2.7-dev", "libpython2.7-stdlib", "libpython2.7-stdlib", "libqt4-opengl", "libqt4-opengl", "libqt4-opengl", "libqt4-opengl-dev", "libqt4-opengl-dev", "libqt5opengl5-dev", "libswscale-dev", "libtbb-dev", "libtbb2", "libzmq3-dev"],
+ ["libjasper-dev", "libjpeg-dev", "libpcre3", "libpython2.7", "libpython2.7", "libpython2.7-dev", "libpython2.7-dev", "libpython2.7-stdlib", "libpython2.7-stdlib", "libqt4-opengl", "libqt4-opengl", "libqt4-opengl", "libqt4-opengl-dev", "libqt4-opengl-dev", "libqt5opengl5-dev", "libreadline-dev", "libswscale-dev", "libtbb-dev", "libtbb2", "libzmq3-dev"],
  ["lm-sensors"].
  ["neovim", "nmap", "ntp", "ntp"],
  ["okular", "okular", "okular", "okular", "okular-extra-backends", "okular-extra-backends"],
@@ -27,7 +27,8 @@ packages = [["automake", "axel"],
  ["tig"],
  ["valgrind", "variety", "vim-gtk", "vim-rails", "vlc"],
  ["xclip", "xkeycaps", "xkeycaps"], 
- ["zeal", "zsh"]
+ ["zeal", "zsh"],
+ ["mysql-server mysql-client libmysqlclient-dev libsqlite3-dev"]
  ]
 
 def change_dir(work):
