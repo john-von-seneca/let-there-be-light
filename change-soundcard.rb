467648a5d21155cb82963cf1ad880a05c2004292
jvs <john.von.seneca@gmail.com>
2016-05-07 10:27:09 +0530

    soundcard stuff

--- change-soundcard.rb
+++ change-soundcard.rb
@@ -17,8 +17,11 @@ rescue ArgumentError => e
 		ix_sink_out = get_index(sound_cards, /DAC/)
 	when "cycle"
 		ix_active_soundcard = get_index(sound_cards, /RUNNING/)
-		ix_other_soundcard = (ix_active_soundcard+1) % sound_cards.size
-		ix_sink_out = ix_other_soundcard
+		ixs_soundcards = sound_cards.map{|sc| sc.match(/index:\s*([0-9])/)[1]}.map(&:to_i)
+		_, ix_ix_active_soundcard = ixs_soundcards.each_with_index.find {|sc, ix| sc==ix_active_soundcard}
+		ix_ix_other_soundcard = (ix_ix_active_soundcard+1) % sound_cards.size
+		ix_sink_out = ixs_soundcards[ix_ix_other_soundcard]
+		puts("active: #{ix_ix_active_soundcard} of #{ixs_soundcards} => #{ix_sink_out}")
 	else
 		raise "Invalid input #{ARGV[0]}"
 	end
--- change-volume.rb
+++ change-volume.rb
@@ -5,14 +5,14 @@ active_sink = get_active_sink_info()
 
 case(ARGV[0])
 when "+"
-	vol = [65536, ((active_sink[:volume] + 10)/100.0) * 65536].min
+	vol = [65536, ((active_sink[:volume] + 10)/100.0) * 65535].min
 when "-"
 	vol = [0, ((active_sink[:volume] - 10)/100.0) * 65536].max
 else
 	raise("what the fuck is this #{ARGV[0]}")
 end
 vol = vol.to_i
-# puts("#{active_sink[:index]} #{vol}")
-# `pacmd set-sink-volume #{active_sink[:index]} #{vol}`
-# puts(get_active_sink_info())
+puts("#{active_sink[:index]} #{active_sink[:volume]} -> #{vol}")
+`pacmd set-sink-volume #{active_sink[:index]} #{vol}`
+puts(get_active_sink_info())
 puts("#{active_sink[:index]} #{vol}")
