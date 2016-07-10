1f864c3f00d3a0075c0fe19be8fc18402e5cd3d2
jvs <john.von.seneca@gmail.com>
2016-07-10 12:48:13 +0530

    linux: apt-adds, apt-add.d, ubuntu-packages

---
+++ linux/apt-add.d/2-apt-add-stack-haskell.sh
@@ -0,0 +1,2 @@
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
+echo 'deb http://download.fpcomplete.com/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/fpco.list
