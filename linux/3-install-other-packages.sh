b90b3032ba024247520450a7d7c2633139566809
jvs <john.von.seneca@gmail.com>
2016-07-31 16:14:07 +0530

    lnx: gnu-global

--- linux/3-install-other-packages.sh
+++ linux/3-install-other-packages.sh
@@ -1,1 +1,9 @@
 npm install -g diff-so-fancy
+
+# https://www.gnu.org/software/global/download.html
+cd ~/neo-human/repos/gnu-src/global/
+cvs update -P -d
+sh reconf.sh 
+./configure
+make -j7
+sudo make install
