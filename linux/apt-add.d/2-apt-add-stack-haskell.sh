4285d9c34f7fbe8fc4bc5b39677e4680950db53b
jvs <john.von.seneca@gmail.com>
2016-07-31 16:14:48 +0530

    lnx/apt-add: haskell

--- linux/apt-add.d/2-apt-add-stack-haskell.sh
+++ linux/apt-add.d/2-apt-add-stack-haskell.sh
@@ -1,2 +1,4 @@
+#https://remusao.github.io/install-ihaskell-on-ubuntu-1404-with-stack.html
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 575159689BEFB442
 echo 'deb http://download.fpcomplete.com/ubuntu xenial main' | sudo tee /etc/apt/sources.list.d/fpco.list
