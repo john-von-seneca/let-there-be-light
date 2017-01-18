2ddd155d8b4a553bfb4206beed5b419f68f6a08a
bebee <balasanjeevi.b@olacabs.com>
2017-01-18 20:09:39 +0530

    tmp

--- git-stuff/gc.sh
+++ git-stuff/gc.sh
@@ -1,3 +1,22 @@
 cd ~/repos
-dirgitcurr=`echo $1 | sed 's/.*:\(.*\).git/\1/'`
-git clone --depth 1 $1 $dirgitcurr
+
+echo $1
+if [[ $1 == https* ]];
+then
+	dirgitcurr=`echo $1 | sed 's/https:\/\/github.com\/\(.*\).git/\1/'`
+fi
+if [[ $1 == git* ]];
+then
+	dirgitcurr=`echo $1 | sed 's/.*:\(.*\).git/\1/'`
+fi
+
+if [[ -z $dirgitcurr ]];
+then
+	echo "dirgitcurr not set"
+	exit 125
+fi
+
+echo $dirgitcurr
+
+# sed 's/https:\/\/github.com\/\(.*\).git/\1/'
+git clone --depth 1 $1 ~/repos/$dirgitcurr
